6357bbda28134abdd152f64fe498604957804c46
jovinvincent <139607641+jovinvincent@users.noreply.github.com>
2026-08-10 07:11:47 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -136,7 +136,8 @@ function parseDerivativeSymbol(cleanSym: string) {
 }
 
 const app = express();
-const PORT = 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10);
+
 
 app.use(express.json());
 
